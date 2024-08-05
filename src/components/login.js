45035591999d99ff7d14b57d284ff4222f2e4767
Aashi pandey <111772050+Aashi002@users.noreply.github.com>
2024-08-05 23:55:14 +0530

    Login.js

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,16 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
+import React, { useContext } from "react";
+import { ThemeContext } from '../App';
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
+  const { theme } = useContext(ThemeContext);
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <div className={`login-page ${theme}`}>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </div>
+  );
 };
 
 export default LoginButton;
