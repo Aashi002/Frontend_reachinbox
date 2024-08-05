3d0dd64c3fd11f0e16382d232094d03e092d028d
Aashi pandey <111772050+Aashi002@users.noreply.github.com>
2024-08-05 23:56:03 +0530

    logout.js

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,13 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
-import Profile from './profile'
+import React, { useContext } from "react";
+import Profile from './Profile' ;
+import { ThemeContext } from '../App';
 
 const LogoutButton = () => {
   const { logout } = useAuth0();
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <div>
-        <button onClick={() => logout()} style={{marginBottom:'20px'}}>Log Out</button >
+    <div className={`logout-page ${theme}`}>
+    <button onClick={() => logout()} style={{ marginBottom: '20px' }}>
+      Log Out
+    </button>
         <Profile/>
     </div>
   );
